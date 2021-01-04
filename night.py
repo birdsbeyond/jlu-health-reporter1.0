1b5acc82b04b9c6bd686b7d5c13ba0ee617ecba0
birdsbeyond <45359689+birdsbeyond@users.noreply.github.com>
2021-01-04 23:32:49 +0800

    Update and rename example.py to night.py

--- night.py
+++ night.py
@@ -5,7 +5,8 @@ start_chrome("https://ehall.jlu.edu.cn/jlu_portal")#界面可见
 write('在此输入用户名', into='请输入邮箱')
 write('在此输入密码', into='请输入密码')
 click(S('#login-submit'))#根据id寻找按钮
-go_to("https://ehall.jlu.edu.cn/jlu_portal/guide?id=2EEF0F5C-F89A-4B8A-88CD-856BF9828F31")
+go_to("https://ehall.jlu.edu.cn/jlu_portal/guide?id=2EFFFFFF-F8FA-4BFA-8FFD-856BF98F8FF1")
+#上面需要复制自己在正常页面下登录后的网址(此处已做混淆处理)
 click(Button("在线办理"))#根据名称寻找按钮
 cell = S(".command_button_content")
 highlight(cell)
